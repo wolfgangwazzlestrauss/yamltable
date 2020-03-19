da0b7f54cdeb7de4b344e9f6cdd78819656f245a
Macklan Weinstein <wolfgangwazzlestrauss@gmail.com>
2020-03-18 19:57:36 -0700

    :whale: Add Docker image for testing.

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -14,7 +14,13 @@ from yamltable import main
 def test_yamltable_version() -> None:
     """Check that all the version tags are in sync."""
 
-    pyproject_path = pathlib.Path(yamltable.__file__).parents[2] / "pyproject.toml"
+    # Check for pyproject.toml in two places in case of nonlocal install.
+    toml_path = pathlib.Path("pyproject.toml")
+    if toml_path.exists():
+        pyproject_path = toml_path
+    else:
+        pyproject_path = pathlib.Path(yamltable.__file__).parents[2] / toml_path
+
     expected = toml.load(pyproject_path)["tool"]["poetry"]["version"]
 
     actual = yamltable.__version__
