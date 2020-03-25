907f5e0ff9cde903b7a777839966d9ab40646470
Macklan Weinstein <wolfgangwazzlestrauss@gmail.com>
2020-03-24 20:04:02 -0700

    :art: Fix small spacing for files

--- docs/src/reference/index.md
+++ docs/src/reference/index.md
@@ -1,4 +1,4 @@
-# API
+# API Reference
 
 While YamlTable was designed for command line usage, its library functions also
 expose helpful functionality. This page gives an overview of all YamlTable
--- src/yamltable/__init__.py
+++ src/yamltable/__init__.py
@@ -1,6 +1,8 @@
 """Command line interface for sorting YAML files."""
 
+
 from yamltable.lib import read, search, sort, validate, write
+
 
 __all__ = ["read", "search", "sort", "validate", "write"]
 __author__ = "Macklan Weinstein"
