3a4aa3846be37f77c20a309d8e55ab3b25dc9558
Macklan Weinstein <wolfgangwazzlestrauss@gmail.com>
2020-10-06 21:24:50 -0700

    :bug: Fix Tox duplicate compiled Python code issue

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,10 +12,8 @@ import yamltable
 def tmp_yaml(tmp_path: pathlib.Path) -> pathlib.Path:
     """Temporary copy of path.yaml."""
 
-    repo_path = pathlib.Path(yamltable.__file__).parents[2]
-
     dest_path = tmp_path / "path.yaml"
-    src_path = repo_path / "tests/data/path.yaml"
+    src_path = pathlib.Path("tests/data/path.yaml")
 
     text = src_path.read_text()
     dest_path.write_text(text)
