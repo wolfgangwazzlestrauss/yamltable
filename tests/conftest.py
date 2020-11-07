a80b778786c9dfa205861e07a08863a9d8436b88
Macklan Weinstein <wolfgangwazzlestrauss@gmail.com>
2020-11-07 09:57:53 -0800

    :wrench: Reorganize test configuration

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,28 @@ import pathlib
 
 import pytest
 
+from yamltable.typing import Schema
+
+
+@pytest.fixture
+def schema(scope: str = "module") -> Schema:
+    """Create reusable JSON schema object.
+
+    Return:
+        JSON schema dictionary
+    """
+
+    return {
+        "$schema": "http://json-schema.org/draft-07/schema#",
+        "type": "object",
+        "properties": {
+            "mock_key_1": {"type": "number"},
+            "mock_key_2": {"type": "number"},
+        },
+        "required": ["mock_key_1", "mock_key_2"],
+        "additionalProperties": False,
+    }
+
 
 @pytest.fixture
 def tmp_yaml(tmp_path: pathlib.Path) -> pathlib.Path:
